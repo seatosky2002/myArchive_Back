0458ea73a07e4aadffdd00900b0bd1a4eb0f6fb9
seatosky2002 <seatosky2002@gmail.com>
2026-05-13 10:59:25 +0900

    fix: CORS 5174 포트 추가, author_nickname backfill 완료

--- config/settings.py
+++ config/settings.py
@@ -166,6 +166,8 @@ REDIS_BLACKLIST_URL = env('REDIS_BLACKLIST_URL', default='redis://localhost:6379
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
